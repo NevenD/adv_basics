fe776267eb4ae1b1c2d711f03c0cbd3a020bfede
Neven Dimač <neven.dimac@gmail.com>
2025-06-04 09:53:17 +0200

    Setting up starting screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 136, 93, 211),
+                const Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
