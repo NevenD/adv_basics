fd7ac77823bb2a5416bb12f03c713ab6ac98715b
Neven Dimač <neven.dimac@gmail.com>
2025-06-04 11:05:58 +0200

    Separating widghets into separate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
-import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 136, 93, 211),
-                const Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 136, 93, 211),
+                const Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    );
+  }
+}
