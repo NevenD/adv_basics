d4f2bbb30bafd038016c76bb5e79f9c9ec71de5b
Neven Dimač <neven.dimac@gmail.com>
2025-06-05 10:27:56 +0200

    Add properties to results screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -43,7 +43,7 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'result-screen') {
-      screenWidget = ResultsScreen();
+      screenWidget = ResultsScreen(choosenAnswers: selectedAnswers);
     }
 
     return MaterialApp(
